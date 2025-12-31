ed50be1b577194a06093a511bfb1b0f56436c821
Rangel Koli <77476841+rangelkoli@users.noreply.github.com>
2025-12-31 18:36:55 -0500

    feat: Add Cloudflare Vite plugin to project dependencies and Vite configuration.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), cloudflare()],
+
 });
