3c1d8e5a8e1f1042eeb18c350ce2d6b63550bedd
Rangel Koli <77476841+rangelkoli@users.noreply.github.com>
2025-12-31 20:34:16 -0500

    chore: remove Cloudflare Vite plugin from configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), cloudflare()],
-
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
